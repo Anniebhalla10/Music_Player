731d85c74beb707bdb40de91a3ad5ce0719132bc
Anniebhalla10 <anniebhalla2001@gmail.com>
2022-06-22 12:13:38 +0530

    progress bar

--- index.js
+++ index.js
@@ -29,6 +29,12 @@ const title = document.getElementById("title");
 const artist = document.getElementById("artist");
 const prev = document.getElementById("previous");
 const next = document.getElementById("next");
+let progress= document.getElementById("progress");
+let total_duration= document.getElementById("duration");
+let total_currentTime=document.getElementById("current_time");
+const progress_div= document.getElementById("progress_div")
+
+
 
 const songs = [
   {
@@ -64,11 +70,46 @@ const nextSong = () => {
 };
 
 const prevSong = () => {
-    songIndex = (songIndex - 1 + songs.length) % songs.length;
-    loadSong(songs[songIndex]);
+  songIndex = (songIndex - 1 + songs.length) % songs.length;
+  loadSong(songs[songIndex]);
   playMusic();
+};
 
-  };
+//  progress bar js functionality
+
+music.addEventListener("timeupdate", (event) => {
+  const {currentTime, duration } = event.srcElement;
+  let progress_time= (currentTime/duration)*100;
+  progress.style.width= `${progress_time}%`;
+
+  //  updating the total time of each song
+  let minute_duration=Math.floor( duration/60);
+  let sec_duration= Math.floor(duration%60);
+
+  if(duration){
+  total_duration.textContent= `${minute_duration}:${sec_duration}`
+  }
+
+  // current duration update
+  let minute_current=Math.floor( currentTime/60);
+  let sec_current= Math.floor(currentTime%60);
+if(sec_current<10){
+  sec_current=`0${sec_current}`
+}
+  total_currentTime.textContent= `${minute_current}:${sec_current}`
+  
+});
+
+// if someone clicks on the progress bar
+
+progress_div.addEventListener("click",(event)=>{
+  const { duration}= music;
+let move_progress= (event.offsetX/ event.srcElement.clientWidth) * duration;
+music.currentTime=move_progress
+});
+
+
+music.addEventListener('ended', nextSong);
 
 next.addEventListener("click", nextSong);
 prev.addEventListener("click", prevSong);
